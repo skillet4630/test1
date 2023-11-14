5fca072ffff105d00e660a3731b876b8d9059ef7
skillet4630 <naver.com>
2023-11-14 17:30:29 +0900

    frist commit

---
+++ Python/스타트코딩/09.클래스/클래스기본개념.py
@@ -0,0 +1,14 @@
+class Monster:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def say(self):
+        print(f'나는 {self.name} {self.age}살임')
+      
+# 객체 = 클래스 이름()  
+shark = Monster('상어', 7)
+wolf = Monster('늑대', 3)
+
+# 객체.메서드() #'.'은 '~의'로 해석하는게 좋음
+shark.say()
+wolf.say()
