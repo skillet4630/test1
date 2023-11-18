5bca574cfd19eff3109068a09aa5226beb6794d0
skillet4630 <kjc4630@naver.com>
2023-11-18 23:29:45 +0900

    first commit

---
+++ Python/스타트코딩/12.크롤링/06.검색어변경하기(하수).py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import pyautogui # 터미널에 pip install pyautogui 설치
+
+keyward = input("검색어를 입력하세요>>>") # input : 파이썬 기본 내장 함수
+response = requests.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=" + keyward) # f스트링
+html = response.text
+
+soup = BeautifulSoup(html , 'html.parser')
+links = soup.select(".news_tit") # 결과는 리스트
+for link in links:
+    title = link.text # 태그 안에 텍스트요소를 가져온다
+    url = link.attrs['href'] # href의 속성값을 가져온다(외워놓기)
+    print(title, url)
