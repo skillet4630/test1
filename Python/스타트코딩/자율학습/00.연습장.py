31ecd8113c7cf34c7fb125a093fc7995b3bb6181
skillet4630 <kjc4630@naver.com>
2023-12-11 03:12:34 +0900

    storage

--- Python/스타트코딩/자율학습/00.연습장.py
+++ Python/스타트코딩/자율학습/00.연습장.py
@@ -1,10 +1,27 @@
-#프로그램 사용자로부터 자연수를 입력 받고,
-#0부터 자연수까지의 합계를 출력하세요.
-num = int(input('자연수를 입력해 주세요>>>'))
+import requests
+from bs4 import BeautifulSoup
 
-sum = 0
- 
-for i in range(1,num+1):
-    sum = sum + i
+def crawl_page(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
 
-print(sum)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup.find_all('a', {'title': lambda x: x and 'Public' in x})
+
+def main():
+    base_url = "https://rargb.to/"
+    search_query = "publicagent 1080p x265"
+    search_url = f"{base_url}search/1/?search={search_query}"
+
+    search_results = crawl_page(search_url)
+
+    if not search_results:
+        print("No results found.")
+        return
+
+    for link in search_results:
+        print(f"Title: {link['title']}")
+
+if __name__ == "__main__":
+    main()
---
+++ Python/스타트코딩/자율학습/06.RARBG 크롤링.py
@@ -0,0 +1,31 @@
+
+import os
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# 결과를 저장할 빈 리스트
+titles = []
+
+for num in range(8):
+    url = f"https://rargb.to/search/{num}/?search=publicagent%201080p%20x265"
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    torrent_links = soup.find_all('a', {'title': lambda x: x and 'Public' in x})
+
+    # 결과를 리스트에 추가
+    for link in torrent_links:
+        titles.append(link['title'])
+
+# 데이터프레임 생성
+df = pd.DataFrame({'Title': titles})
+
+# 결과 파일 경로 설정
+output_path = r'C:\Users\kjc46\OneDrive\바탕 화면\컴퓨터 공부\Python\Python\스타트코딩\자율학습\PublicAgent.xlsx'
+
+# Excel 파일로 저장
+df.to_excel(output_path, index=False)
+
+# 결과 메시지 출력
+print(f"Results saved to: {output_path}")
