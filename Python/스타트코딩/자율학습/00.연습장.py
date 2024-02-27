eb8f4c6269b8a2b2ba9ac1457e61532fb538fb6b
skillet4630 <kjc4630@naver.com>
2024-02-27 22:52:47 +0900

    first commit

--- Python/스타트코딩/자율학습/00.연습장.py
+++ Python/스타트코딩/자율학습/00.연습장.py
@@ -1,20 +1,6 @@
-import random 
+#변수선언 유의사항 텍스트를 넣고싶을땐 ""꼭 넣기
+level = 5
+power = 50
+health = 100
 
-lotto_num = [] #빈 로또 번호 리스트 생성
-
-def getRandomNumber():
-    number = random.randint(1,45)
-    return number
-
-count = 0 # 횟수를 저장할 변수
-
-#무한반복
-while True:
-    if count > 5:
-        break
-    random_number = getRandomNumber() # 로또 번호 하나를 뽑는다
-    if random_number not in lotto_num: # 로또 번호 리스트 안에 뽑은 로또 번호가 없으면
-        lotto_num.append(random_number) # 로또 번호 히스트에 뽑은 로또 번호를 추가해라
-        count = count + 1
-
-print(lotto_num)
+print(level, power, health)
